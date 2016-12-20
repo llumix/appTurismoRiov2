a4f595633b580c08be3132854d5610ecb86cc80f
dpinta <dppinta@gmail.com>
2016-12-20 17:58:16 -0500

    splash, menú principal

--- app/src/main/java/com/riobamba/appturismorio/MainActivity.java
+++ app/src/main/java/com/riobamba/appturismorio/MainActivity.java
@@ -2,10 +2,14 @@ package com.riobamba.appturismorio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.riobamba.appturismorio.data.dbAplication;
+import com.riobamba.appturismorio.model.MenuAdapter;
 import com.riobamba.appturismorio.model.entity.cMenu;
 
 import java.util.ArrayList;
@@ -14,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView tabla1;
     private ArrayList<String> nombres;
-
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +35,35 @@ public class MainActivity extends AppCompatActivity {
          tabla1.setAdapter(adapter);*/
 
         ArrayList<cMenu> itemsMenu = new ArrayList<>();
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Ruta Urbano Patrimonial", ""));
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Ruta Chimborazo", ""));
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Ruta Puertas del Altar", ""));
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Riobamba Ferroviaria", ""));
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Rata de las Iglesias", ""));
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Terminales Terrestres", ""));
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Realidad Aumentada", ""));
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Identidad y Tradición", ""));
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Puntos de Información", ""));
-        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_iglesia), "Acerca de", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Ruta Urbano Patrimonial", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Ruta Chimborazo", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Ruta Puertas del Altar", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Riobamba Ferroviaria", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Rata de las Iglesias", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Terminales Terrestres", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Realidad Aumentada", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Identidad y Tradición", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Puntos de Información", ""));
+        itemsMenu.add( new cMenu(getResources().getDrawable(R.mipmap.logo_riobamba), "Acerca de", ""));
 
+        lista= (ListView) findViewById(R.id.tabla1);
+        lista.setAdapter(new MenuAdapter(itemsMenu, R.layout.menu_principal_list_row, this)
+        {
+            @Override
+            public void onEntrada(Object entrada, View view) {
+                if (entrada != null) {
+                    TextView texto_superior_entrada = (TextView) view.findViewById(R.id.txtvopcion);
+                    if (texto_superior_entrada != null)
+                        texto_superior_entrada.setText(((cMenu) entrada).getTexto1());
+
+
+
+                    ImageView imagen_entrada = (ImageView) view.findViewById(R.id.imgvlogo);
+                    if (imagen_entrada != null)
+                        imagen_entrada.setImageDrawable(((cMenu) entrada).getImagen());
+                }
+            }
+        });
 
     }
 }
